3fc92ff79da9b57e7611dbd279874b736dea4fc8
justavanee <avaneelakare@gmail.com>
2025-03-29 18:12:07 +0530

    base website

---
+++ lab4/script.js
@@ -0,0 +1,9 @@
+function claimFreeCourse() {
+    alert("🎁 You've successfully claimed your free course! Check your email for access.");
+  }
+  
+  function submitForm(event) {
+    event.preventDefault();
+    alert("✅ Your message has been sent. We'll get back to you shortly.");
+  }
+  
